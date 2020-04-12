54e5d9ea69a77366a0a2d46e408f767cb5fd8b1e
Kevin Olson <kevin.olson07@gmail.com>
2020-04-12 15:21:04 -0700

    fixed the key error by having the server check for the type of method

--- server.py
+++ server.py
@@ -4,17 +4,32 @@ app = Flask(__name__)
 #bycrypt = bycrypt(app) 
 app.secret_key = "keep it secret, keep it safe"
 
-@app.route('/')
+user = {
+    "username":"kevinolson07" }
+# print(user['username'])
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    print("you are in the index function!!!", flush=True)
-    return render_template("index.html")
+    if request.method == "POST":
+        print("you are in the index function!!!", flush=True)
+        print(user["username"], flush=True)
+        userN = request.form["username"]
+        print(userN, flush=True)
+        if userN == user["username"]:
+            return render_template('home.html')
+        else:
+            return redirect("/")
+    # uname = request.form["uname"]
+    # if(uname== user["username"]):
+    #     print("you are in the home function!!!")
+    #     return render_template("home.html", data=30)
+    else:
+        return render_template("index.html")
 
-@app.route('/home')
-def home():
-    print("you are in the home function!!!")
-    return render_template("home.html", data=30)
-
-
+@app.route('/registration')
+def registration():
+    print("you are in the registation function!!!", flush=True)
+    return render_template("registration.html")
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port=8090)
